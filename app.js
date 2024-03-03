a9822d5b87e7f4fe363225756a454a1153725fb9
Eugene <dubstepsj@mail.ru>
2024-03-03 12:47:42 +1000

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+function popup() {
+  document.getElementById("popup").style.zIndex = "9";
+  document.getElementById("popup").style.opacity = "1";
+}
+function popupclose() {
+  document.getElementById("popup").style.zIndex = "-10";
+  document.getElementById("popup").style.opacity = "0";
+}
